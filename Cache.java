aec5a8cfea68d05e718fb0d7907d1b668a396b63
Elyviere <elyviere@elyviere@gmail.com>
2017-02-23 19:48:24 +0100

    Added addVideo

--- Cache.java
+++ Cache.java
@@ -1,12 +1,25 @@
-public class Cache {
-  int cap;
-  Endpoint[] endpoints;
-  int[] videos;
+import java.util.ArrayList;
 
-  public Cache(int cap, Endpoint[] endpoints, int[] videos){
+public class Cache {
+  int cap, remaining;
+  Endpoint[] endpoints;
+  int[] videoSizes;
+  ArrayList<Integer> storedVideos;
+
+  public Cache(int cap, Endpoint[] endpoints, int[] videoSizes){
 
     this.cap = cap;
+    remaining = cap;
     this.endpoints = endpoints;
-    this.videos = videos;
+    this.videoSizes = videoSizes;
+    storedVideos = new ArrayList<>();
   }
+
+  public boolean addVideo(int id){
+    if (videoSizes[id] < remaining){
+      remaining -= videoSizes[id];
+      storedVideos.add(id);
+      return true;
+    }
+    return false;
 }
